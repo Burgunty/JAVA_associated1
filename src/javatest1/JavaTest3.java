c4e8804735e550730bc9a2bfd48c3e28ee1010ed
XS170202104101 <2632662919@qq.com>
2019-05-07 14:50:00 +0800

    My java test project1

---
+++ src/javatest1/JavaTest3.java
@@ -0,0 +1,7 @@
+package javatest1;
+
+public class JavaTest3 {
+	public static void main(String[] args){
+		System.out.println("Yani");
+	}
+}
---
+++ src/javatest1/JavaTest4.java
@@ -0,0 +1,7 @@
+package javatest1;
+
+public class JavaTest4 {
+	public static void main(String[] args){
+		System.out.println("I love China ");
+	}
+}
